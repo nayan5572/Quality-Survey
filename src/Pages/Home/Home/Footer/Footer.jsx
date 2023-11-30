2638b500b8c261177fa2ec38ac2618db4325f54e
nayan <nayan.dev572@gmail.com>
2023-11-30 22:32:53 +0600

    added user

--- src/Pages/Home/Home/Footer/Footer.jsx
+++ src/Pages/Home/Home/Footer/Footer.jsx
@@ -1,5 +1,7 @@
+import moment from "moment";
 
 const Footer = () => {
+    const currentYear = moment().year();
     return (
         <footer className="px-4 divide-y bg-gray-800 text-gray-100 py-8">
             <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
@@ -10,7 +12,7 @@ const Footer = () => {
                                 <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
                             </svg>
                         </div>
-                        <span className="self-center text-2xl font-semibold">Brand name</span>
+                        <span className="self-center text-2xl font-semibold">Quality Serve</span>
                     </a>
                 </div>
                 <div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
@@ -78,7 +80,7 @@ const Footer = () => {
                     </div>
                 </div>
             </div>
-            <div className="py-6 text-sm text-center dark:text-gray-400">© 1968 Company Co. All rights reserved.</div>
+            <div className="py-6 text-sm text-center dark:text-gray-400"> <p>@ {currentYear} Quality Serve. All rights reserved.</p></div>
         </footer>
     );
 };
