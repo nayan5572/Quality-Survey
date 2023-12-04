b2ff2f0bbb27cc8ed01a40166453c6815a4d9afe
nayan <nayan.dev572@gmail.com>
2023-12-04 18:03:59 +0600

    create survey

--- src/Layout/SurveyCreation/SurveyCreation.jsx
+++ src/Layout/SurveyCreation/SurveyCreation.jsx
@@ -1,85 +1,122 @@
 import Swal from "sweetalert2";
-// import useAxiosPublic from './../../components/Hooks/useAxiosPublic';
+import useAxiosPublic from './../../components/Hooks/useAxiosPublic';
+import useAxiosSecure from "../../components/Hooks/useAxiosSecure";
+import { useForm } from "react-hook-form";
+import { FaUtensils } from "react-icons/fa";
 
 
 
-
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY;
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`;
 
 const SurveyCreation = () => {
     // const axiosPublic = useAxiosPublic();
     // const navigate = useNavigate();
 
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const form = e.target;
-        const description = form.description.value;
-        const title = form.title.value;
-        const category = form.category.value;
+    const { register, handleSubmit, reset } = useForm();
+    const axiosPublic = useAxiosPublic();
+    const axiosSecure = useAxiosSecure();
 
-        const myData = {
-            description,
-            title,
-            category
-        }
-
-        console.log("My Data is", myData);
-
-        fetch('https://assignment-12-server-one-tan.vercel.app/featuredSurvey', {
-            method: 'POST',
+    const onSubmit = async (data) => {
+        console.log(data);
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
             headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(myData)
-        })
-            .then(res => res.json())
-            .then(data => {
-                if (data.insertedId) {
-                    Swal.fire({
-                        position: "top",
-                        icon: "success",
-                        title: "Your Survey Created done",
-                        showConfirmButton: false,
-                        timer: 1500
-                    });
-                }
-            })
-    }
+                'content-type': 'multipart/form-data'
+            }
+        });
+        if (res.data.success) {
+            // now send the menu item data to the server with the image
+            const menuItem = {
+                name: data.name,
+                category: data.category,
+                price: parseFloat(data.price),
+                recipe: data.recipe,
+                image: res.data.data.display_url
+            }
+            const menuRes = await axiosSecure.post('/menu', menuItem);
+            console.log(menuRes.data);
+            if (menuRes.data.insertedId) {
+                // show success popup
+                reset();
+                Swal.fire({
+                    position: "top-end",
+                    icon: "success",
+                    title: `${data.name} is added to the survey`,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+            }
+        }
+        console.log(res.data);
+    };
 
 
     return (
-        <div className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
-            <h2 className="text-2xl font-semibold mb-4">Create a Survey</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-700">Title</label>
-                    <input
-                        type="text"
-                        name="title"
-                        className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-sm font-medium text-gray-700">Description</label>
-                    <textarea
-                        name="description"
-                        className="mt-1 p-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring focus:border-blue-500"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="category">Category:</label>
-                    <select className="w-full border rounded-md p-2" name="category" required>
-                        <option value="">Select:</option>
-                        <option value="health">Health</option>
-                        <option value="sports">Sports</option>
-                        <option value="technology">Technology</option>
-                        <option value="entertainment">Entertainment</option>
-                        <option value="food">Food</option>
-                        {/* Add more categories as needed */}
-                    </select>
-                </div>
-                <input className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-700" type="submit" value="Create Survey" />
-            </form>
+        <div>
+            <div>
+                <form onSubmit={handleSubmit(onSubmit)} className="max-w-md mx-auto p-6 bg-white rounded-md shadow-md">
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600">
+                            Survey Name <span className="text-red-600">*</span>
+                        </label>
+                        <input
+                            type="text"
+                            placeholder="Survey Name"
+                            {...register('name', { required: true })}
+                            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+                        />
+                    </div>
+
+                    <div className="flex gap-4">
+                        {/* Category */}
+                        <div className="flex-1 mb-4">
+                            <label className="block text-sm font-medium text-gray-600">
+                                Category <span className="text-red-600">*</span>
+                            </label>
+                            <select
+                                defaultValue="default"
+                                {...register('category', { required: true })}
+                                className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+                            >
+                                <option disabled value="default">
+                                    Select a Category
+                                </option>
+                                <option value="health">Health</option>
+                                <option value="sports">Sports</option>
+                                <option value="food">Food</option>
+                                <option value="entertainment">Entertainment</option>
+                                <option value="technology">Technology</option>
+                                <option value="science">Science</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    {/* Recipe Details */}
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600">Description</label>
+                        <textarea
+                            {...register('recipe', { required: true })}
+                            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+                            placeholder="Survey Details"
+                        ></textarea>
+                    </div>
+
+                    <div className="mb-4">
+                        <label className="block text-sm font-medium text-gray-600">Image</label>
+                        <input
+                            {...register('image', { required: true })}
+                            type="file"
+                            className="mt-1 p-2 border border-gray-300 rounded-md w-full focus:outline-none focus:border-blue-500"
+                        />
+                    </div>
+
+                    <button className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">
+                        Add Survey
+                    </button>
+                </form>
+            </div>
         </div>
     );
 };
